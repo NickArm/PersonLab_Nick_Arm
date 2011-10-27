cb3c4761cc20f0a8581365ebd73a60209ec7e93f
Nick <armenisnick@hotmail.com>
2011-10-27 08:05:31 -0700

    translate twitter

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,7 +4,7 @@ require "twitter"
 # virtual model not have database table
 class Tweet < ActiveRecord::Base
   
-  def self.gets(uid = 'huacnlee',count = 5,force = false)
+  def self.gets(uid = 'Nick_Arm',count = 5,force = false)
     msgs = Rails.cache.read("data/tweet/#{uid}")
     if not msgs or force      
       msgs = Twitter.user_timeline(uid, :count => count)
